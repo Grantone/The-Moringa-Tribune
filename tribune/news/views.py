ee354aef4f41a26c018695c04d324b8b580dcddf
Granton Mchana <gmchana254@gmail.com>
2017-11-21 16:37:24 +0300

    detication

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -4,7 +4,7 @@ import datetime as dt
 from .models import Article
 from .forms import NewsLetterForm
 from .email import send_welcome_email
-from django.contrib.auth.decorators import login_required.
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
