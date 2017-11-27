8f1ef4e0912c56bcb3d049b92593a9220eb5fecd
Granton Mchana <gmchana254@gmail.com>
2017-11-27 14:30:52 +0300

    Fixing bugs

--- tribune/news/models.py
+++ tribune/news/models.py
@@ -6,22 +6,6 @@ from tinymce.models import HTMLField
 # Create your models here.
 
 
-class Editor(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=10, blank=True)
-
-    def __str__(self):
-        return self.first_name
-
-    def save_editor(self):
-        self.save()
-
-    class Meta:
-        ordering = ['first_name']
-
-
 class tags(models.Model):
     name = models.CharField(max_length=30)
 
